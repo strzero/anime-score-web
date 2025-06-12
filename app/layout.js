ba0a9bb5fb871b871c3b9d1940e7ba8ebd9ff5a1
StellateZero <stellatezero@outlook.com>
2025-06-12 17:07:39 +0800

    update: 移除提示

--- app/layout.js
+++ app/layout.js
@@ -24,7 +24,7 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Header />
-        <Notification />
+        {/* <Notification /> */}
         {children}
       </body>
     </html>
