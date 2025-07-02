5e2abba5a91743fe45ae8adff9fc8844fe5c96e4
StellateZero <stellatezero@outlook.com>
2025-07-02 13:04:29 +0800

    update: 添加 Google Analytics 追踪代码

--- app/layout.js
+++ app/layout.js
@@ -20,6 +20,16 @@ export let metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-LRSXSFDBYN"></script>
+        <script dangerouslySetInnerHTML={{ __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-LRSXSFDBYN');
+        ` }} />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
